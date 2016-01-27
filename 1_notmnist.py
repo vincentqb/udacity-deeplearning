8326559017623014643ff0f947f6ea89258614ee
Vincent QB <vincentqb@users.noreply.github.com>
2016-01-27 15:02:59 -0500

    did first part of homework.

--- 1_notmnist.py
+++ 1_notmnist.py
@@ -36,6 +36,9 @@ def maybe_download(filename, expected_bytes):
 train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
 test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
 
+"""
+Extract the dataset from the compressed .tar.gz file. This should give you a set of directories, labelled A through J.
+"""
 
 num_classes = 10
 
@@ -57,3 +60,69 @@ def extract(filename):
   
 train_folders = extract(train_filename)
 test_folders = extract(test_filename)
+
+"""
+Problem 1
+Let's take a peek at some of the data to make sure it looks sensible. Each exemplar should be an image of a character A through J rendered in a different font. Display a sample of the images that we just downloaded. Hint: you can use the package IPython.display.
+"""
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os, random
+
+letter = random.choice('ABCDEFJ')
+folder_name = 'notMNIST_small/' + letter + '/'
+file_name = folder_name + random.choice(os.listdir(folder_name))
+img = mpimg.imread(file_name)
+plt.imshow(img)
+plt.show()
+
+"""
+Now let's load the data in a more manageable format.
+We'll convert the entire dataset into a 3D array (image index, x, y) of floating point values, normalized to have approximately zero mean and standard deviation ~0.5 to make training easier down the road. The labels will be stored into a separate array of integers 0 through 9.
+A few images might not be readable, we'll just skip them.
+"""
+
+def load(data_folders, min_num_images, max_num_images):
+  dataset = np.ndarray(
+    shape=(max_num_images, image_size, image_size), dtype=np.float32)
+  labels = np.ndarray(shape=(max_num_images), dtype=np.int32)
+  label_index = 0
+  image_index = 0
+  for folder in data_folders:
+    print(folder)
+    for image in os.listdir(folder):
+      if image_index >= max_num_images:
+        raise Exception('More images than expected: %d >= %d' % (
+          image_index, max_num_images))
+      image_file = os.path.join(folder, image)
+      try:
+        image_data = (ndimage.imread(image_file).astype(float) -
+                      pixel_depth / 2) / pixel_depth
+        if image_data.shape != (image_size, image_size):
+          raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+        dataset[image_index, :, :] = image_data
+        labels[image_index] = label_index
+        image_index += 1
+      except IOError as e:
+        print('Could not read:', image_file, ':', e, '- it\'s ok, skipping.')
+    label_index += 1
+  num_images = image_index
+  dataset = dataset[0:num_images, :, :]
+  labels = labels[0:num_images]
+  if num_images < min_num_images:
+    raise Exception('Many fewer images than expected: %d < %d' % (
+        num_images, min_num_images))
+  print('Full dataset tensor:', dataset.shape)
+  print('Mean:', np.mean(dataset))
+  print('Standard deviation:', np.std(dataset))
+  print('Labels:', labels.shape)
+  return dataset, labels
+
+train_dataset, train_labels = load(train_folders, 450000, 550000)
+test_dataset, test_labels = load(test_folders, 18000, 20000)
+
+"""
+Problem 2
+Let's verify that the data still looks good. Displaying a sample of the labels and images from the ndarray. Hint: you can use matplotlib.pyplot.
+"""
